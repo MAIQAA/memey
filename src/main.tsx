f2cdf64a1be10e74962d5e56791eb69ffa85be30
Ammad Rana <ammadrana863@gmail.com>
2025-04-10 04:15:50 +0500

    changes made

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 // font Inter
 import "@fontsource/inter/300.css";
 import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css"; 
+import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/800.css";
 // Font Pacifico
 import "@fontsource/pacifico/400.css";
-import { Navbar } from './components/Navbar.tsx';
-import Footer from './components/Footer.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { Navbar } from "./components/Navbar.tsx";
+import Footer from "./components/Footer.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <Navbar/>
-    <App />
-    <Footer/>
+      <Navbar />
+      <App />
+      <Footer />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
